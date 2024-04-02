cb7f47af6abe329ca9bfbbbe1cadc5750ac84377
Ankur Chauhan <115866837+ankurchauhanOK@users.noreply.github.com>
2024-04-02 09:59:35 +0000

    variable

---
+++ .devcontainer/basics_01/datatypes.js
@@ -0,0 +1,25 @@
+// alert(3+3) // We are using nodejs, not browser
+console.log(3+3);
+console.log("Ankur");
+let name = "Ankur"
+let age = 21
+let Logged_In = true
+
+/* datatypes :
+number: 2 to power 53
+bigint
+string => ""
+boolean => True / Flase
+null => Standalone value
+undefined => 
+Symbol
+
+
+Object
+*/
+
+console.log(typeof "Ankur");
+console.log(typeof null); // datatype is object
+console.log(typeof undefined); // datatype is undefined
+// typeof is used to identify the type of datatype.
+console.log("The datatype of age is",typeof age);
---
+++ .devcontainer/basics_01/variable.js
@@ -0,0 +1,12 @@
+const account_Id = 12345
+let Acc_username = "Ankur Chauhan"
+var Acc_password = "12345" // not to use
+location_ = "Mumbai"
+let Acc_detail;
+console.log(Acc_username);
+
+Acc_username = "Battu"
+Acc_password = "Voodoo"
+location_ = "Rampur" 
+
+console.table([account_Id, Acc_username, Acc_password,location_, Acc_detail])
