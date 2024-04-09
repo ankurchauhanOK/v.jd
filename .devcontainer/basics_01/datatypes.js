8eeab58dea26aaf2168b4efdce0035b6bcf79ef4
Ankur Chauhan <115866837+ankurchauhanOK@users.noreply.github.com>
2024-04-09 06:10:19 +0000

    convwesion of datatypes

--- .devcontainer/basics_01/datatypes.js
+++ .devcontainer/basics_01/datatypes.js
@@ -23,3 +23,28 @@ console.log(typeof null); // datatype is object
 console.log(typeof undefined); // datatype is undefined
 // typeof is used to identify the type of datatype.
 console.log("The datatype of age is",typeof age);
+
+// 1 and 0 to boolean true and false.
+
+let roku1 = 1
+
+let tosh1 = Boolean(roku1)
+console.log(tosh1);
+
+let roku2 = 0
+
+let tosh2 = Boolean(roku2)
+console.log(tosh2);
+
+// 1 = true, 0 = false
+// " " = false
+// "Ankur" = true
+
+// number to string conversion
+
+let somenum = 44
+
+let somechar = String(somenum)
+
+console.log(somechar);
+console.log(typeof(somechar));
