3f32fe20cf2642691557462b5e3a21e5a14055c4
Ankur Chauhan <115866837+ankurchauhanOK@users.noreply.github.com>
2024-06-04 11:55:26 +0000

    comparision

---
+++ .devcontainer/basics_01/comparision.js
@@ -0,0 +1,9 @@
+console. log (null > 0);
+console. log (null == 0);
+console. log (null >= 0); 
+console. log (undefined == 0); 
+console. log (undefined > 0);
+console. log (undefined < 0);
+
+// "===" is used for differentiate between datatype of the data
+console. log ("2" === 2);
