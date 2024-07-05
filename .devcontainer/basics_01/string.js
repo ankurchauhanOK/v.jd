ee9dc09be375b9a4679f0dd8e3744063894aff38
Ankur Chauhan <115866837+ankurchauhanOK@users.noreply.github.com>
2024-07-05 20:02:09 +0000

    strings

---
+++ .devcontainer/basics_01/string.js
@@ -0,0 +1,17 @@
+let name = "Ankur Chauhan";
+const age = 21;
+
+console.log(`My name is ${name} and my age is ${age}.`);
+
+const newGame = new String('ANkur-chua-han')
+
+console.log(`game name is ${newGame}`);
+console.log(`game name is ${newGame.toUpperCase()}`);
+console.log(`game name is ${newGame.charAt(2)}`);
+console.log(`game name is ${newGame.indexOf('k')}`);
+const newstring = newGame.substring(0,3)
+console.log(`${newstring}`);
+
+const ankur = "My name is ankur";
+console.log(`${ankur.replace('ankur', 'Cutetauji')}`);
+console.log(`${newGame.split('-')}`);
