4784a1efa51dbbfaca51024341cb784298071ca6
Ankur Chauhan <115866837+ankurchauhanOK@users.noreply.github.com>
2024-07-06 03:25:41 +0000

    math&num

---
+++ .devcontainer/basics_01/num_math.js
@@ -0,0 +1,34 @@
+// const score = 400;
+// console.log(score);
+
+// const value = new Number(300);
+
+// console.log(value);
+// console.log(value.toFixed(2));
+// console.log(score.toFixed);
+
+// const newnumber = 224.6554;
+
+// console.log(newnumber.toPrecision(3));
+// const nmber = 1000000000
+// console.log(`${nmber.toLocaleString('en-IN')}`);
+
+
+//+++++++++++++++++++++++. MATHS. +++++++++++++++++++++++++++++++++++++
+
+// console.log(Math.abs(-200));
+// console.log(Math.round(4.5));
+// console.log(Math.round(4.4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.1));
+// console.log(Math.floor(4.99));
+// console.log(Math.max(4 , 5, 8, 9));
+// console.log(Math.min(4 , 5, 8, 9));
+
+// console.log(Math.random());
+// console.log(Math.floor((Math.random()*10)+1));
+
+const max = 20;
+const min = 10;
+
+console.log(Math.floor((Math.random()*(max-min + 1))) + min);
