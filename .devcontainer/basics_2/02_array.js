ef31a8d98279028d11c34540bf8a7b4fdb4e7879
Ankur Chauhan <115866837+ankurchauhanOK@users.noreply.github.com>
2024-07-07 18:21:13 +0000

    2array

--- .devcontainer/basics_2/02_array.js
+++ .devcontainer/basics_2/02_array.js
@@ -1,22 +1,22 @@
 const marvel_heroes = ["Spiderman","Hulk","ironman"];
 const dc_heroes = ["Superman","Batman","Wonderwomen"];
 
-// marvel_heroes.push(dc_heroes)
-// console.log(marvel_heroes[3][0])
+marvel_heroes.push(dc_heroes)
+console.log(marvel_heroes[3][0])
 
-// const ham = marvel_heroes.concat(dc_heroes);
-// console.log(ham);
+const ham = marvel_heroes.concat(dc_heroes);
+console.log(ham);
 
-// const newnewhero = [...marvel_heroes, ...dc_heroes]
+const newnewhero = [...marvel_heroes, ...dc_heroes]
 
-// console.log(newnewhero);
+console.log(newnewhero);
 
-// const arroi = [1,2,3,4,[6,7,8,[2,49,8]],[2,3,4,5]]
-// const arro = arroi.flat(Infinity)
-// console.log(arro);
+const arroi = [1,2,3,4,[6,7,8,[2,49,8]],[2,3,4,5]]
+const arro = arroi.flat(Infinity)
+console.log(arro);
 
-// console.log((Array.isArray("Ankur")))
-// console.log((Array.from("Ankur")))
+console.log((Array.isArray("Ankur")))
+console.log((Array.from("Ankur")))
 
 let Anmol=Array.from("Ankur")
 //  console.log(Array.isArray(Anmol));
