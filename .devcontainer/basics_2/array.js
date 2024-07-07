ea16022ee0bb28fb76a2f8f200bee0356dab7467
Ankur Chauhan <115866837+ankurchauhanOK@users.noreply.github.com>
2024-07-07 11:50:33 +0000

    array

---
+++ .devcontainer/basics_2/array.js
@@ -0,0 +1,21 @@
+let arr =[1,2,3,4,5,6]
+// arr.push(9)
+// arr.pop()
+// arr.unshift(9)
+// arr.shift()
+// console.log(arr)
+// console.log(arr.includes(9))
+// console.log(arr.indexOf(5));
+
+// let new_array = arr.join()
+// console.log(new_array);
+
+console.log(`Original array is: ${arr}`);
+
+let arr3 = arr.slice(1,3)
+console.log(`${arr}`);
+console.log(`Value in slice is: ${arr3}`)
+
+let arr2 = arr.splice(1,4)
+console.log(`${arr}`);
+console.log(`The array after splice is: ${arr2}`);
