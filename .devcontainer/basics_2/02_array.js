18ebf107d1c8b4630dc4966d7b8d70774773c987
Ankur Chauhan <115866837+ankurchauhanOK@users.noreply.github.com>
2024-07-07 18:19:48 +0000

    array_02

---
+++ .devcontainer/basics_2/02_array.js
@@ -0,0 +1,28 @@
+const marvel_heroes = ["Spiderman","Hulk","ironman"];
+const dc_heroes = ["Superman","Batman","Wonderwomen"];
+
+// marvel_heroes.push(dc_heroes)
+// console.log(marvel_heroes[3][0])
+
+// const ham = marvel_heroes.concat(dc_heroes);
+// console.log(ham);
+
+// const newnewhero = [...marvel_heroes, ...dc_heroes]
+
+// console.log(newnewhero);
+
+// const arroi = [1,2,3,4,[6,7,8,[2,49,8]],[2,3,4,5]]
+// const arro = arroi.flat(Infinity)
+// console.log(arro);
+
+// console.log((Array.isArray("Ankur")))
+// console.log((Array.from("Ankur")))
+
+let Anmol=Array.from("Ankur")
+//  console.log(Array.isArray(Anmol));
+ console.log(Array.from({name: "Ankur"}))
+
+ let score = 100;
+ let score1 = 1000;
+ let score2 = 10000;
+console.log(Array.of(score,score1,score2));
