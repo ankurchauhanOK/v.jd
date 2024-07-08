daa9b09da1a86d2080c9c58ec2e79d37a9116708
Ankur Chauhan <115866837+ankurchauhanOK@users.noreply.github.com>
2024-07-08 16:30:04 +0000

    object.js

--- .devcontainer/basics_2/object.js
+++ .devcontainer/basics_2/object.js
@@ -1,23 +1,29 @@
-// const name2 = Symbol ("hkey")
+const name2 = Symbol ("hkey")
 
 const obj1 = {
-    // name : "ankur chuahan",
-    // college : "ABESIT",
-    // [name2]: "hkey",
-    // age: 21,
-    // Place : "Uttarakhand",
-    // Days : ["SUnday", "Monday", "Tuesday", "wednesday", "thursday", "friday", "sat"]
+    name : "ankur chuahan",
+    college : "ABESIT",
+    [name2]: "hkey",
+    age: 21,
+    Place : "Uttarakhand",
+    Days : ["SUnday", "Monday", "Tuesday", "wednesday", "thursday", "friday", "sat"]
     }
 // console.log(obj1["name"]);
 // console.log(obj1["age"]);
 // console.log(obj1[name2]);
 
-// obj1.name = "Ankur Kumar Chauhan"
+obj1.name = "Ankur Kumar Chauhan"
 // Object.freeze(obj1); 
 
 console.log(obj1);
 
 obj1.hola = function(){
     console.log("Ankur Chauhan")
-}
-console.log(obj1.hola())
+};
+
+obj1.hola1 = function(){
+    console.log(`My name is, ${this.name}`);
+};
+
+console.log(obj1.hola());
+console.log(obj1.hola1());
