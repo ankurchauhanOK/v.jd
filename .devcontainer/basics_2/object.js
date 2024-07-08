5d10cd067bb6a317ed1959ecfbe67d64f1548c4a
Ankur Chauhan <115866837+ankurchauhanOK@users.noreply.github.com>
2024-07-08 11:01:14 +0000

    object_basics

---
+++ .devcontainer/basics_2/object.js
@@ -0,0 +1,23 @@
+// const name2 = Symbol ("hkey")
+
+const obj1 = {
+    // name : "ankur chuahan",
+    // college : "ABESIT",
+    // [name2]: "hkey",
+    // age: 21,
+    // Place : "Uttarakhand",
+    // Days : ["SUnday", "Monday", "Tuesday", "wednesday", "thursday", "friday", "sat"]
+    }
+// console.log(obj1["name"]);
+// console.log(obj1["age"]);
+// console.log(obj1[name2]);
+
+// obj1.name = "Ankur Kumar Chauhan"
+// Object.freeze(obj1); 
+
+console.log(obj1);
+
+obj1.hola = function(){
+    console.log("Ankur Chauhan")
+}
+console.log(obj1.hola())
